ebc6b11a8c3c98f4cacfe644e12c10a6e4e06968
Jungdayoon <dayoon98@naver.com>
2020-11-02 18:41:28 +0900

    Quick Sort

---
+++ 알고리즘 정리/QuickSort.py
@@ -0,0 +1,34 @@
+def quickSort(s, e, step):
+    step += 1
+
+    pivot = unsortedArr[s]  # 맨 앞의 value 로 설정
+    bs = s
+    be = e
+    print("step{}: pivot: {}, arr: {}".format(step, pivot, unsortedArr))
+    while s < e:
+        while unsortedArr[e] >= pivot and s < e:
+            e -= 1
+        if s > e:
+            break
+        while unsortedArr[s] <= pivot and s < e:
+            s += 1
+        if s > e:
+            break
+        unsortedArr[s], unsortedArr[e] = unsortedArr[e], unsortedArr[s]
+
+    unsortedArr[bs], unsortedArr[s] = unsortedArr[s], unsortedArr[bs]
+    if bs < s:
+        quickSort(bs, s-1, step)
+    if be > e:
+        quickSort(s+1, be, step)
+
+
+
+unsortedArr = [int(x) for x in input().split()]
+
+print("**** Quick Sort ****")
+step = 0
+
+quickSort(0, len(unsortedArr)-1, step)
+
+
