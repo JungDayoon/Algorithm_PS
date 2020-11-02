e11edbb179cb9823680929ffe92770a3f5d1ea71
Jungdayoon <dayoon98@naver.com>
2020-11-02 17:33:43 +0900

    Insertion Sort

---
+++ 알고리즘 정리/InsertionSort.py
@@ -0,0 +1,11 @@
+unsortedArr = [int(x) for x in input().split()]
+
+for i in range(1, len(unsortedArr)):
+    compare = unsortedArr[i]
+    j = i-1
+    while j >= 0 and compare < unsortedArr[j]:
+        unsortedArr[j+1] = unsortedArr[j]
+        j -= 1
+    unsortedArr[j+1] = compare
+    print("step{}: {}".format(i, unsortedArr))
+
